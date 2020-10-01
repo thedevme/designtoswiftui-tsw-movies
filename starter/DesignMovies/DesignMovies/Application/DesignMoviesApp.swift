41c799efaf4ff11ff53efeae16a51ab1a03fa18f
Craig <craig.clayton@adeptmobi.com>
2020-10-01 14:59:54 -0400

    starter project

---
+++ starter/DesignMovies/DesignMovies/Application/DesignMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignMoviesApp.swift
+//  DesignMovies
+//
+//  Created by Craig Clayton on 9/22/20.
+//
+
+import SwiftUI
+
+@main
+struct DesignMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
